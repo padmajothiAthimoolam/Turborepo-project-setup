689fe2f569bc64f7e705dc2629233bd6733b6074
Padmajothi <pathimoolam@prometheussoftware.ca>
2024-09-26 23:10:23 -0400

    Turborepo project setup and added Tailwind configuration setup

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button"; 
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+      
+
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.logo}
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+            Read our docs
+          </a>
+        </div>
+        <Button appName="web" className={styles.secondary}>
+          Open alert
+        </Button>
+      </main>
+
+    </div>
+  );
+}
---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,2 @@
+const config = require('@repo/tailwind-config/package.json')
+module.exports = config;
